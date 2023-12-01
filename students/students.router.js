d15358d96b3053c8648bff0c655152c94616474a
FernandoGmz2001 <fernandogmz2001@gmail.com>
2023-11-30 22:09:23 -0600

    feat: A lot of changes

--- students/students.router.js
+++ students/students.router.js
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import redoc from 'redoc-express'
 import {
   createNewStudent,
   deleteStudentById,
@@ -44,11 +45,30 @@ studentRouter.get("/alumnos", async (_, res) => {
  *          description: Regresa un usuario en especifico
  * 
  */
+
+
 studentRouter.get("/alumnos/:id", async (req, res) => {
   const { id } = req.params;
   const result = await getStudentById(id);
   res.json(result[0]);
 });
+
+/**
+ * @swagger
+ * /alumnos/{id}:
+ *    delete:
+ *      tags:
+ *        - Estudiantes
+ *      summary: Eliminar un estudiante por su id
+ *      description: Elimina un estudiante mediante su id
+ *      parameters:
+ *        - in: path
+ *          name: id
+ *      responses:
+ *        200:
+ *          description: Elimina un estudiante especifico
+ * 
+ */
 
 studentRouter.delete("/alumnos/:id", async (req, res) => {
   const { id } = req.params;
@@ -56,10 +76,41 @@ studentRouter.delete("/alumnos/:id", async (req, res) => {
   res.json(result[0]);
 });
 
+/**
+ * @swagger
+ * /alumnos/new:
+ *    post:
+ *      tags:
+ *        - Estudiantes
+ *      summary: Crear un nuevo estudiante
+ *      description: Se crea un nuevo estudiante
+ *      responses:
+ *        200:
+ *          description: Creacion de un nuevo estudiante
+ * 
+ */
+
 studentRouter.post("/alumnos/new", async (req, res) => {
   const result = await createNewStudent(req);
   res.json(result[0]);
 });
+
+/**
+ * @swagger
+ * /alumnos/{id}:
+ *    put:
+ *      tags:
+ *        - Estudiantes
+ *      summary: Edita un estudiante por su id
+ *      description: Edita un estudiante mediante su id
+ *      parameters:
+ *        - in: path
+ *          name: id
+ *      responses:
+ *        200:
+ *          description: Se edita un estudiante en especifico
+ * 
+ */
 
 studentRouter.put("/alumnos/:id", async (req, res) => {
   const result = await updateStudent(req);
