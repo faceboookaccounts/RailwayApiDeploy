e663156d541ee15e8dec308a7be949f1191dcee6
FernandoGmz2001 <fernandogmz2001@gmail.com>
2023-11-30 22:35:17 -0600

    Changes

---
+++ config.js
@@ -0,0 +1,5 @@
+export const PORT = process.env.PORT || 8080;
+export const HOST = process.env.HOST || 'localhost';
+export const DB = process.env.DB || 'ApiDatabase';
+export const DB_USER = process.env.DB_USER || 'root';
+export const DB_PASSWORD = process.env.DB_PASSWORD || '1234';
--- connection.js
+++ connection.js
@@ -1,10 +1,11 @@
 import mysql from 'mysql2';
+import { DB,DB_PASSWORD,DB_USER,PORT,HOST } from './config.js';
 
 export const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: '1234', 
-  database: 'ApiDatabase',
+  host: HOST,
+  user: DB_USER,
+  password: DB_PASSWORD, 
+  database: DB,
   waitForConnections: true,
   connectionLimit: 10,
   maxIdle: 10, // max idle connections, the default value is the same as `connectionLimit`
